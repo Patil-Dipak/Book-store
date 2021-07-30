2b3fb82d602c900779f72e4cb682e2b8b2afb74f
Patil-Dipak <dips281999@gmail.com>
2021-07-30 17:42:48 +0530

    created product detail view and it's template

--- core/settings.py
+++ core/settings.py
@@ -64,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # for show categories function to all templates 
+                'store.views.categories', 
             ],
         },
     },
--- store/views.py
+++ store/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 # Create your views here.
 
-
+# For return all products information
 def all_products(request):
     products = Product.objects.all()
     
     return render(request, 'store/home.html', {'products':products})
 
+# For return the all categories
+def categories(request):
+    return {
+            'categories' : Category.objects.all()
+           }
+
+# For product details
+def product_detail(request, slug):
+    product =  get_object_or_404(Product, slug= slug, in_stock = True)
+    return render(request, 'store/product/detail.html', {'product':product})
