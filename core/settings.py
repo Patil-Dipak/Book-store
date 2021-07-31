fbabde99df4f1f0c0a0e7b75798234f785d50a5a
Patil-Dipak <dips281999@gmail.com>
2021-07-31 17:18:37 +0530

    created new app basket

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    # User define app
+    'store',
+    'basket'
 ]
 
 MIDDLEWARE = [
