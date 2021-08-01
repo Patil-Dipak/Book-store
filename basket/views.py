ce477243d9a54e28246b2baf6af9458cb76f840c
Patil-Dipak <dips281999@gmail.com>
2021-08-01 12:05:21 +0530

    changed in bakset's urls and views create new method basket_add

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .basket import Basket
 
 # Create your views here.
 
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+
+def basket_add(request):
+    basket = Basket(request)    #object of Basket class 
+    if request.POST.get('action') == 'post':    # check for post methods
+        product_id = int(request.POST.get('productid')) # get id from the json code 
+        product = get_object_or_404(Product, id = product_id)
+        basket.add(product = product)   # called the add method of basket class 
