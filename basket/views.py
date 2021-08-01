5cc4025ca5964477507f7e467c941478374909b7
Patil-Dipak <dips281999@gmail.com>
2021-08-01 16:31:06 +0530

    added the new parameter in basket.add function call qty

--- basket/views.py
+++ basket/views.py
@@ -15,8 +15,9 @@ def basket_add(request):
     basket = Basket(request)    #object of Basket class 
     if request.POST.get('action') == 'post':    # check for post methods
         product_id = int(request.POST.get('productid')) # get id from the json code 
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id = product_id)
-        basket.add(product = product)   # called the add method of basket class 
+        basket.add(product = product, qty = product_qty)   # called the add method of basket class 
 
         response = JsonResponse({'test':'data'})
         return response
