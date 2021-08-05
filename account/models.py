6587b78aa8cc0a5efbf02eb58659eed2c9ae20d9
Patil-Dipak <dips281999@gmail.com>
2021-08-06 00:04:06 +0530

    Created the UserBase model in models.py of account

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+from django_countries.fields import CountryField
+
+class UserBase(AbstractBaseUser, PerPermissionsMixin):
+    email = models.EmailField(_('email address'), unique= True)
+    user_name = models.CharField(max_length=255, unique =  True)
+    first_name = models.CharField(max_length = 255, blank = True)
+    about = models.TextField(_('about'), max_length = 500, blank = True)
+
+    #Delivery details
+    country = ContryField()
