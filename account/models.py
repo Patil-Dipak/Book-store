14089dcaef1ca6f997f1a37db7ae203813f8cf18
Patil-Dipak <dips281999@gmail.com>
2021-08-06 08:58:53 +0530

    created the CustomAccountManager in models of account

--- account/models.py
+++ account/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 from django_countries.fields import CountryField
+from django.core.mail import send_mail
+from django.utils.translation import gettext_lazy as _
 
 class UserBase(AbstractBaseUser, PerPermissionsMixin):
     email = models.EmailField(_('email address'), unique= True)
@@ -36,3 +38,28 @@ class UserBase(AbstractBaseUser, PerPermissionsMixin):
 
     def __str__(self):
         return self.user_name
+
+class CustomAccountManager(BaseUserManager):
+    def create_superuser(self,email, user_name,password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise valueError('Superuser must be assigned to is_staff = True')
+
+        if other_fields.get('is_superuser') is not True:
+            raise valueError('Superuser must be assigned to is_superuser = True')
+
+        return self.create_user(email, user_name, password, **other_fields)
+    
+    def create_user(self, email, user_name, password, **other_fields):
+
+        if not email:
+            raise valueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email = email, user_name = user_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
