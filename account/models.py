334ac85f087384c98745054b83496e32aa7ebea0
Patil-Dipak <dips281999@gmail.com>
2021-08-06 08:40:15 +0530

    Added new fileds in UserBase

--- account/models.py
+++ account/models.py
@@ -13,3 +13,26 @@ class UserBase(AbstractBaseUser, PerPermissionsMixin):
 
     #Delivery details
     country = ContryField()
+    phone_number = models.CharFields(max_length = 15, blank = True)
+    postcode = models.CharFields(max_length = 12, blank = True)
+    address_line_1 = models.CharFields(max_length = 150, blank = True)
+    address_line_2 = models.CharFields(max_length = 150, blank = True)
+    town_city = models.CharFields(max_length = 150, blank = True)
+
+    #user Status
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    class Meta:
+        verbose_name = 'Accounts'
+        verbose_name_plural = 'Accounts'
+
+    def __str__(self):
+        return self.user_name
