9c6797f0f290df17366fb15eacbea9d2bdb9fcae
Patil-Dipak <dips281999@gmail.com>
2021-08-06 09:15:52 +0530

    changed in models.py

--- account/models.py
+++ account/models.py
@@ -7,37 +7,6 @@ from django_countries.fields import CountryField
 from django.core.mail import send_mail
 from django.utils.translation import gettext_lazy as _
 
-class UserBase(AbstractBaseUser, PerPermissionsMixin):
-    email = models.EmailField(_('email address'), unique= True)
-    user_name = models.CharField(max_length=255, unique =  True)
-    first_name = models.CharField(max_length = 255, blank = True)
-    about = models.TextField(_('about'), max_length = 500, blank = True)
-
-    #Delivery details
-    country = ContryField()
-    phone_number = models.CharFields(max_length = 15, blank = True)
-    postcode = models.CharFields(max_length = 12, blank = True)
-    address_line_1 = models.CharFields(max_length = 150, blank = True)
-    address_line_2 = models.CharFields(max_length = 150, blank = True)
-    town_city = models.CharFields(max_length = 150, blank = True)
-
-    #user Status
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    objects = CustomAccountManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name']
-
-    class Meta:
-        verbose_name = 'Accounts'
-        verbose_name_plural = 'Accounts'
-
-    def __str__(self):
-        return self.user_name
 
 class CustomAccountManager(BaseUserManager):
     def create_superuser(self,email, user_name,password, **other_fields):
@@ -63,3 +32,35 @@ class CustomAccountManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique= True)
+    user_name = models.CharField(max_length=255, unique =  True)
+    first_name = models.CharField(max_length = 255, blank = True)
+    about = models.TextField(_('about'), max_length = 500, blank = True)
+
+    #Delivery details
+    country = CountryField()
+    phone_number = models.CharField(max_length = 15, blank = True)
+    postcode = models.CharField(max_length = 12, blank = True)
+    address_line_1 = models.CharField(max_length = 150, blank = True)
+    address_line_2 = models.CharField(max_length = 150, blank = True)
+    town_city = models.CharField(max_length = 150, blank = True)
+
+    #user Status
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    class Meta:
+        verbose_name = 'Accounts'
+        verbose_name_plural = 'Accounts'
+
+    def __str__(self):
+        return self.user_name
