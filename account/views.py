fd9331e2b6fa96522a9ce66cbfd67b82c2843673
Patil-Dipak <dips281999@gmail.com>
2021-08-08 17:56:06 +0530

    Added the account_register method in views.py of account app

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import  RegistrationForm
+
+def account_register(request):
+
+    if request.user.is_authenticated:
+        return redirect('/')
+    
+    if request.method == "POST":
+        registrationForm = RegistrationForm(request.POST)
+
+        if registrationForm.is_valid(): # Check registration is valid or not
+            user = registrationForm.save(commit = False)
+            user.email = registrationForm.cleaned_data['email']
+            user.set_password(registrationForm.cleaned_data['password'])
+            user.is_active = False  # set user active as false
+            user.save() # save the user data
