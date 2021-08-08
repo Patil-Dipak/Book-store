ae3bc42da79245b38a28579a161edf000edb14d4
Patil-Dipak <dips281999@gmail.com>
2021-08-08 18:55:42 +0530

    Added the setup email in account register method in views.py of account app

--- account/views.py
+++ account/views.py
@@ -16,3 +16,16 @@ def account_register(request):
             user.set_password(registrationForm.cleaned_data['password'])
             user.is_active = False  # set user active as false
             user.save() # save the user data
+
+            # setup email
+
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user':user,
+                'domain': current_site.domain,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : account_activation_token.make_token(user),
+            })
+
+            user.email_user(subject = subject, message = message)
