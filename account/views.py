e78d92da22de130dff0cec66048907c8e58244b1
Patil-Dipak <dips281999@gmail.com>
2021-08-09 22:38:52 +0530

    changed in views of account app

--- account/views.py
+++ account/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import redirect, render
-from django.http import HttpResponse
-
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
-from .forms import RegistrationForm
-from .tokens import account_activation_token
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
+#from orders.views import user_orders
+
+from .forms import RegistrationForm #, UserEditForm
 from .models import UserBase
+from .tokens import account_activation_token
 
 @login_required
 def dashboard(request):
@@ -23,33 +23,28 @@ def account_register(request):
     if request.user.is_authenticated:
         return redirect('/')
     
-    if request.method == "POST":
-        registrationForm = RegistrationForm(request.POST)
-
-        if registrationForm.is_valid(): # Check registration is valid or not
-            user = registrationForm.save(commit = False)
-            user.email = registrationForm.cleaned_data['email']
-            user.set_password(registrationForm.cleaned_data['password'])
-            user.is_active = False  # set user active as false
-            user.save() # save the user data
-
-            # setup email
-
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False
+            user.save()
             current_site = get_current_site(request)
             subject = 'Activate your Account'
             message = render_to_string('account/registration/account_activation_email.html', {
-                'user':user,
+                'user': user,
                 'domain': current_site.domain,
-                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
-                'token' : account_activation_token.make_token(user),
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
             })
-
-            user.email_user(subject = subject, message = message)
-    
+            user.email_user(subject=subject, message=message)
+            return HttpResponse('registered succesfully and activation sent')
     # method is not Post
     else:
-        registrationForm = RegistrationForm()
-        return render(request, 'account/registration/register.html', {'form': registrationForm})
+        registerForm = RegistrationForm()
+    return render(request, 'account/registration/register.html', {'form': registerForm})
 
 def account_activate(request, uidb64, token):
     try:
