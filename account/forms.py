459fe5e608875c740b5e6eb06671e2ccafae7c2e
Patil-Dipak <dips281999@gmail.com>
2021-08-09 22:35:59 +0530

    Changed in forms of accout app

--- account/forms.py
+++ account/forms.py
@@ -5,44 +5,39 @@ from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm,
 
 # Registration Form
 class RegistrationForm(forms.ModelForm):
-    # for User name
-    user_name = forms.CharField(label = 'Enter Username', min_length = 4 , max_length = 50 , help_text = 'Required')
-    # For email
-    email = forms.EmailField(max_length = 50 , help_text = 'Required', error_messages= {
-        'required' : 'Sorry, you will need an email'})
-    # for Password
-    Password = forms.CharField(label = 'Password', widget= forms.PasswordInput)
-    Password2 = forms.CharField(label = 'Repeat password', widget= forms.PasswordInput)
 
-    # Meta Class
-    model = UserBase
-    fields = ('user_name', 'email', )
+    user_name = forms.CharField(
+        label='Enter Username', min_length=4, max_length=50, help_text='Required')
+    email = forms.EmailField(max_length=100, help_text='Required', error_messages={
+        'required': 'Sorry, you will need an email'})
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = UserBase
+        fields = ('user_name', 'email',)
 
     def clean_username(self):
         user_name = self.cleaned_data['user_name'].lower()
-        r = UserBase.objects.filter(user_name= user_name) # return the data
-
-        if r.count(): # if count = 1
+        r = UserBase.objects.filter(user_name=user_name)
+        if r.count():
             raise forms.ValidationError("Username already exists")
+        return user_name
 
-        return user_name    # if count not one then return user_name
-
-    # for Checking the password
     def clean_password2(self):
-        cd = self.cleaned_data # store the method into cd
+        cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError("Passwords does not match.")
-        
+            raise forms.ValidationError('Passwords do not match.')
         return cd['password2']
 
-    
-    # for Checking the email
     def clean_email(self):
         email = self.cleaned_data['email']
-        if UserBase.objects.filter(email = email).exists():
-            raise forms.ValidationError("Please use another Email, that is already taken")
+        if UserBase.objects.filter(email=email).exists():
+            raise forms.ValidationError(
+                'Please use another Email, that is already taken')
         return email
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['user_name'].widget.attrs.update(
