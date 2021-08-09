c182a1437b524b03e015b44d1687c2ee94131f42
Patil-Dipak <dips281999@gmail.com>
2021-08-09 17:34:44 +0530

    Added the user dashboard path

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,7 @@ app_name = "account"
 urlpatterns = [
         path('register/', views.account_register,  name = 'register'),
         path('activate/<slug:uidb64>/<slug:token>/', views.account_activate,  name = 'activate'),
-
+        #user dashboard
+        path('dashboard/', views.dashboard,  name = 'dashboard'),
 
 ]
