915e4e96503b6e57c70c7f515d1616523a4fc77e
Patil-Dipak <dips281999@gmail.com>
2021-08-10 09:35:06 +0530

    Added the namespace in urls of account app

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "account"
+app_name ='account'
 
 urlpatterns = [
         path('register/', views.account_register,  name = 'register'),
