dfdfe15485645906d8bec3b7a2c32d2837340f74
Patil-Dipak <dips281999@gmail.com>
2021-08-12 19:04:43 +0530

    Added the login url in urls.py of account

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views # for login and logout
+from .forms import (UserLoginForm)
 
 app_name ='account'
 
@@ -8,5 +10,6 @@ urlpatterns = [
         path('activate/<slug:uidb64>/<slug:token>/', views.account_activate,  name = 'activate'),
         #user dashboard
         path('dashboard/', views.dashboard,  name = 'dashboard'),
-
+        path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html',
+                                                form_class=UserLoginForm), name='login'),
 ]
