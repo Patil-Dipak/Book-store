e730da7ff58d7880cdc3a037fd6f0b7ce141e188
Patil-Dipak <dips281999@gmail.com>
2021-08-12 23:16:12 +0530

    Added the edit profile url in urls.py in account app

--- account/urls.py
+++ account/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
         path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html',
                                                 form_class=UserLoginForm), name='login'),
         path('logout/', auth_views.LogoutView.as_view(next_page='/account/login/'), name='logout'),
+        path('profile/edit/', views.edit_details,  name ='edit_details'),
 
 ]
