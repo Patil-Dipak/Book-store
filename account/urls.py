bc70076c178b3561eea5c4509b5f0919df017afd
Patil-Dipak <dips281999@gmail.com>
2021-08-12 22:24:36 +0530

    Added the logout url in urls.py of account app

--- account/urls.py
+++ account/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
         path('dashboard/', views.dashboard,  name = 'dashboard'),
         path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html',
                                                 form_class=UserLoginForm), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(next_page='/account/login/'), name='logout'),
+
 ]
