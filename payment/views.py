d2c15556bd6af5d2827d37d30ab0f57ca4a0ed3a
Patil-Dipak <dips281999@gmail.com>
2021-08-16 23:04:29 +0530

    Changed in Basketview method in views.py of payment

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from basket.basket import Basket    # for the use Basket class 
 
 # Basket view
 
 @login_required     # only if user is login
 def BasketView(request):
+    basket = Basket(request)
+    total = str(basket.get_total_price())
+    total = total.replace('.', '')
+    total = int(total)
+
     return render(request, 'payment/home.html')
