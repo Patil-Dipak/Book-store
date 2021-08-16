23f14dc9c0979c674da6b807dbbec4ac2f5c6e61
Patil-Dipak <dips281999@gmail.com>
2021-08-16 08:57:34 +0530

    Added the BasketView method in views.py of payment

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Basket view
+
+@login_required     # only if user is login
+def BasketView(request):
+    return render(request, 'payment/home.html')
